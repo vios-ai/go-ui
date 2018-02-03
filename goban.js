ee68564d89aa8378b9478494f6d7cbbbc941df66
Laurent Demailly <ldemailly@gmail.com>
2018-02-03 13:46:08 -0800

    Tickle the cache

--- goban.js
+++ goban.js
@@ -8,7 +8,7 @@ var Stones = {
 }
 
 var DEBUG = false
-var VERSION = '0.3.3'
+var VERSION = '0.3.3' // Don't we love caching issues...
 
 // Class encapsulating the logic for a Go Game (valid games, capture, history
 // sgf import/export, etc...)
